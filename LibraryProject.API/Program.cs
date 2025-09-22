1324ce4a41e63350de0c8208f53b300b1b7cbabe
Jose Diego Mariano de Oliveira Passos <mirante.jpassos@terceiros.mag.com.br>
2025-09-22 09:55:51 -0300

    Configure Swagger

--- LibraryProject.API/Program.cs
+++ LibraryProject.API/Program.cs
@@ -85,17 +85,16 @@ builder.Services.AddMediatR(cfg =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Biblioteca API v1");
+    });
 }
 
 app.UseHttpsRedirection();
